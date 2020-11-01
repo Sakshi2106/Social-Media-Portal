cf9183dab0c80cc9ba9d3b5fd6789b7db1291f77
RiddhiGhate <riddhighate.07@gmail.com>
2020-11-01 17:41:29 +0530

    final

--- feed/models.py
+++ feed/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.utils import timezone
 
 class Post(models.Model):
-	description = models.CharField(max_length=255, blank=True)
+	caption = models.CharField(max_length=255, blank=True)
 	pic = models.ImageField(upload_to='path/to/img')
 	date_posted = models.DateTimeField(default=timezone.now)
 	user_name = models.ForeignKey(User, on_delete=models.CASCADE)
